4d99aaa32bba357400aa120d5d4b0a60faf066e6
Jordy van Ekelen <jordy.vanekelen@nldm-7zn2.home>
2020-11-28 18:23:45 +0100

    Setup base project

---
+++ LambdaCode/app.py
@@ -0,0 +1,15 @@
+import json
+
+# import requests
+
+
+def lambda_handler(event, context):
+    # Create IP List parser object
+
+    # Fetch IP lists, TODO: One class handle all, regex from https://github.com/awslabs/aws-waf-security-automations/blob/master/source/reputation_lists_parser/reputation-lists.py
+
+    # parse all lists, cross check maybe?
+
+    # Waf update with lists (get token there)
+
+    pass
---
+++ LambdaCode/connection/http_get.py
@@ -0,0 +1,3 @@
+class HTTPGet:
+    """ This class is reponsible for making requests to the IP list parser provider URL's and returning the result. """
+
